418060dff89def4434ac5e29bd321fe76f8354a9
sunagatov43 <79836948+sunagatov43@users.noreply.github.com>
2021-12-18 22:18:27 +0500

    Update index.php

--- php-sunagatov/index.php
+++ php-sunagatov/index.php
@@ -19,40 +19,43 @@
  $d=date("d.m.Y H:i");
  echo($d);
 ?>
+<p> Ссылки на Лабораторную работу №1:
+<p>
 <ul>
-<li>
-<a href='/lab-1-1.php'>lab-1-1</a>
-</li>
-<li>
-<a href='/lab-1-2.php'>lab-1-2</a>
-</li>
-<li>
-<a href='/lab-1-3.php'>lab-1-3</a>
-<li> 
-<a href='/lab1-5.php'>lab1-5</a>
-<li>
-<a href='/lab-2-1.php'>lab-2-1</a>
-<li>
-<a href='/lab-2-2.php'>lab-2-2</a>
-<li>
-<a href='/lab-2-3.php'>lab-2-3</a>
-<li>
-<a href='/lab-2-4.php'>lab-2-4</a>
-<li>
-<a href='/lab-2-5.php'>lab-2-5</a>
-<li>
-<a href='/lab-2-6.php'>lab-2-6</a>
-<li>
-<a href='/lab-2-7.php'>lab-2-7</a>
-<li>
-<a href='/lab-2-8.php'>lab-2-8</a>
-<li>
-<a href='/lab-2-9.php'>lab-2-9</a>
-<li>
-<a href='/lab-2-10.php'>lab-2-10</a>
-<li>
-<a href='/lab-2-11.php'>lab-2-11</a>
-<ul>
+<li><a href="lab-1-1.php">Упражнение 1-2</a></li>
+<li><a href="lab-1-2.php">Упражнение 1-3</a></li>
+<li><a href="lab-1-3.php">Упражнение 1-4</a></li>
+<li><a href="lab1-5.php">Упражнение 1-5</a></li>
+<li><a href="lab-2-1.php">Упражнение 1-6</a></li>
+<li><a href="lab-2-2.php">Упражнение 1-7</a></li>
+<li><a href="lab-2-3.php">Упражнение 1-8</a></li>
+<li><a href="lab-2-4.php">Упражнение 1-9</a></li>
+<li><a href="lab-2-5.php">Упражнение 1-10</a></li>
+<li><a href="lab-2-6.php">Упражнение 1-11</a></li>
+<li><a href="lab-2-7.php">Упражнение 1-12</a></li>
+<li><a href="lab-2-8.php">Упражнение 1-13</a></li>
+<li><a href="lab-2-9.php">Упражнение 1-14</a></li>
+<li><a href="lab-2-10.php">Задача №1-2</a></li>
+<li><a href="lab-2-11.php">Задача №1-3</a></li>
+
+<p>Ссылки на Лабораторную работу №2:
+<p>
+
+<li><a href="lab-21.php">Задание 2-1</a></li>
+<li><a href="lab-22.php">Задание 2-2</a></li>
+<li><a href="lab-23.php">Задание 2-3</a></li>
+<li><a href="lab-24.php">Задача 2-4</a></li>
+<li><a href="lab-25.php">Задача 2-5</a></li>
+
+
+</ul>
+
+<?php
+require_once "lab-26.php";
+
+
+?>
+
 </div>
 
 
