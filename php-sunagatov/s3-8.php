16362226fdcacc3f005264093589bd92667d528b
sunagatov43 <79836948+sunagatov43@users.noreply.github.com>
2021-12-26 02:18:44 +0500

    исправлена кодировка

--- php-sunagatov/s3-8.php
+++ php-sunagatov/s3-8.php
@@ -1,6 +1,6 @@
 <?php
 foreach ($_POST as $key => $string) {
-  $_POST[$key] = iconv("cp1251", "UTF-8", $string);
+  $_POST[$key] = iconv("UTF-8", "UTF-8", $string);
 }
 if (empty($_POST["text2"])) {
           echo "Заполните поле";
