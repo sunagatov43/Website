24cb011837485b0051644cd625cf4f1eca4fb35a
sunagatov43 <79836948+sunagatov43@users.noreply.github.com>
2021-12-30 00:07:34 +0500

    Add files via upload

--- php-sunagatov/index.php
+++ php-sunagatov/index.php
@@ -1,61 +1,95 @@
-<p> Работу выполнил: Сунагатов И.Р., ПИ-321
-<p> 
-<p> Дата и время:
-<p> 
-<?php
- $d=date("d.m.Y H:i");
- echo($d);
-?>
-<p> Ссылки на Лабораторную работу №1:
-<p>
-<ul>
-<li><a href="lab-1-1.php">Упражнение 1-2</a></li>
-<li><a href="lab-1-2.php">Упражнение 1-3</a></li>
-<li><a href="lab-1-3.php">Упражнение 1-4</a></li>
-<li><a href="lab1-5.php">Упражнение 1-5</a></li>
-<li><a href="lab-2-1.php">Упражнение 1-6</a></li>
-<li><a href="lab-2-2.php">Упражнение 1-7</a></li>
-<li><a href="lab-2-3.php">Упражнение 1-8</a></li>
-<li><a href="lab-2-4.php">Упражнение 1-9</a></li>
-<li><a href="lab-2-5.php">Упражнение 1-10</a></li>
-<li><a href="lab-2-6.php">Упражнение 1-11</a></li>
-<li><a href="lab-2-7.php">Упражнение 1-12</a></li>
-<li><a href="lab-2-8.php">Упражнение 1-13</a></li>
-<li><a href="lab-2-9.php">Упражнение 1-14</a></li>
-<li><a href="lab-2-10.php">Задача №1-2</a></li>
-<li><a href="lab-2-11.php">Задача №1-3</a></li>
-
-<p>Ссылки на Лабораторную работу №2:
-<p>
-
-<li><a href="lab-21.php">Задание 2-1</a></li>
-<li><a href="lab-22.php">Задание 2-2</a></li>
-<li><a href="lab-23.php">Задание 2-3</a></li>
-<li><a href="lab-24.php">Задача 2-4</a></li>
-<li><a href="lab-25.php">Задача 2-5</a></li>
-
-
-</ul>
-
-<?php
-require_once "lab-26.php";
-
-?>
-<p>Ссылки на Лабораторную работу №3:
-<p>
-
-<li><a href="f1_get.php">Упражнение 3-1</a></li>
-<li><a href="f2_post.php">Упражнение 3-2</a></li>
-<li><a href="f3_p.php">Упражнение 3-3</a></li>
-<li><a href="f4_p.php">Упражнение 3-4</a></li>
-<li><a href="f5_p.php">Упражнение 3-5</a></li>
-<li><a href="f6_p.php">Упражнение 3-6</a></li>
-<li><a href="f7.php">Упражнение 3-7</a></li>
-<li><a href="s3-1.php">Задача 3-1</a></li>
-<li><a href="s3-2.php">Задача 3-2</a></li>
-<li><a href="s3-3.php">Задача 3-3</a></li>
-<li><a href="s3-4.php">Задача 3-4</a></li>
-<li><a href="s3-5.php">Задача 3-5</a></li>
-<li><a href="s3-6.html">Задача 3-6</a></li>
-
-</ul>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Сунагатов Ильгиз</title>
+<link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="design/design321.css">
+
+
+</head>
+
+<body>
+<div class = "styles">
+<p> Работу выполнил: Сунагатов И.Р., ПИ-321
+<p> 
+<p> Дата и время:
+<p> 
+<?php
+ $d=date("d.m.Y H:i");
+ echo($d);
+?>
+<p> Ссылки на Лабораторную работу №1:
+<p>
+<ul>
+<li><a href="lab-1-1.php">Упражнение 1-2</a></li>
+<li><a href="lab-1-2.php">Упражнение 1-3</a></li>
+<li><a href="lab-1-3.php">Упражнение 1-4</a></li>
+<li><a href="lab1-5.php">Упражнение 1-5</a></li>
+<li><a href="lab-2-1.php">Упражнение 1-6</a></li>
+<li><a href="lab-2-2.php">Упражнение 1-7</a></li>
+<li><a href="lab-2-3.php">Упражнение 1-8</a></li>
+<li><a href="lab-2-4.php">Упражнение 1-9</a></li>
+<li><a href="lab-2-5.php">Упражнение 1-10</a></li>
+<li><a href="lab-2-6.php">Упражнение 1-11</a></li>
+<li><a href="lab-2-7.php">Упражнение 1-12</a></li>
+<li><a href="lab-2-8.php">Упражнение 1-13</a></li>
+<li><a href="lab-2-9.php">Упражнение 1-14</a></li>
+<li><a href="lab-2-10.php">Задача №1-2</a></li>
+<li><a href="lab-2-11.php">Задача №1-3</a></li>
+
+<p>Ссылки на Лабораторную работу №2:
+<p>
+
+<li><a href="lab-21.php">Задание 2-1</a></li>
+<li><a href="lab-22.php">Задание 2-2</a></li>
+<li><a href="lab-23.php">Задание 2-3</a></li>
+<li><a href="lab-24.php">Задача 2-4</a></li>
+<li><a href="lab-25.php">Задача 2-5</a></li>
+
+
+</ul>
+
+<?php
+require_once "lab-26.php";
+
+?>
+<p>Ссылки на Лабораторную работу №3:
+<p>
+
+<li><a href="f1_get.php">Упражнение 3-1</a></li>
+<li><a href="f2_post.php">Упражнение 3-2</a></li>
+<li><a href="f3_p.php">Упражнение 3-3</a></li>
+<li><a href="f4_p.php">Упражнение 3-4</a></li>
+<li><a href="f5_p.php">Упражнение 3-5</a></li>
+<li><a href="f6_p.php">Упражнение 3-6</a></li>
+<li><a href="f7.php">Упражнение 3-7</a></li>
+<li><a href="s3-1.php">Задача 3-1</a></li>
+<li><a href="s3-2.php">Задача 3-2</a></li>
+<li><a href="s3-3.php">Задача 3-3</a></li>
+<li><a href="s3-4.php">Задача 3-4</a></li>
+<li><a href="s3-5.php">Задача 3-5</a></li>
+<li><a href="s3-6.html">Задача 3-6</a></li>
+
+</ul>
+
+<?php
+require_once "lab-26.php";
+
+?>
+<p>Ссылки на Лабораторную работу №4:
+<p>
+    <li><a href="bd_user/index.php">bd user</a></li>
+    <li><a href="bd_auto/index2.php">bd auto</a></li>
+    
+    <p>Ссылки на Лабораторную работу №5:
+<p>
+    <li><a href="bd_auto/Autorization/login.php">Вход</a></li>
+    <li><a href="bd_auto/Autorization/signup.php">Регистрация</a></li>
+    
+</div>
+
+
+<body>
+</html>
+
